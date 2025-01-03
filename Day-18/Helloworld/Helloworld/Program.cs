e3b8b9d2f66e38235ab0052b8fc3abbf39799367
jayapreethi <jayapreethi.sridhar@example.com>
2025-01-03 12:41:52 +0530

    Hi

---
+++ Day-18/Helloworld/Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Assignment
+{
+    class HelloWorld
+
+    {
+        public static void Main()
+        {
+            for (int i = 0; i <= 10; i++)
+                Console.WriteLine("Hello World");
+        }
+    }
+}
+
