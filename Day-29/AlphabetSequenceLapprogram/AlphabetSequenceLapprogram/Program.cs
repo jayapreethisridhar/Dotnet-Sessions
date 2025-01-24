ebad69121d1c43c6979c968171de8df45cccbde1
jayapreethi <jayapreethi.sridhar@example.com>
2025-01-24 09:51:29 +0530

    Day-29

---
+++ Day-29/AlphabetSequenceLapprogram/AlphabetSequenceLapprogram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace AlphabetSequenceLapprogram
+{
+    public class Program
+    {
+        static bool isAlphabetOrder(String s)
+        {
+            int n = s.Length;
+            char[] c = new char[n];
+            for (int i = 0; i < n; i++)
+            {
+                c[i] = s[i];
+
+            }
+            Array.Sort(c);
+            for (int i = 0; i < n; i++)
+            {
+                if (c[i] != s[i])
+                    return false;
+
+            }
+            return true;
+        }
+        public static void Main()
+        {
+            String s = "abcdef";
+            if (isAlphabetOrder(s))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("NO");
+
+        }
+    }
+}
