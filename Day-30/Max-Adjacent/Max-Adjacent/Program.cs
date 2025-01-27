74ed7fc2f8213fb2dc1a04c83b1d8d2147a1db1f
jayapreethi <jayapreethi.sridhar@example.com>
2025-01-27 10:15:43 +0530

    Day-30

---
+++ Day-30/Max-Adjacent/Max-Adjacent/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Max2AdjacentFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 7, 8, 10, 6 };
+
+            int maxProduct = FindMaxAdjacentProduct(array);
+
+            Console.WriteLine("The maximum product of two adjacent elements is: " + maxProduct);
+        }
+
+        static int FindMaxAdjacentProduct(int[] array)
+        {
+            int maxProduct = int.MinValue;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int product = array[i] * array[i + 1];
+
+                if (product > maxProduct)
+                {
+                    maxProduct = product;
+                }
+            }
+            return maxProduct;
+        }
+    }
+}
