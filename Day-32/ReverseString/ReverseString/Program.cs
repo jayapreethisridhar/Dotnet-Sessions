5208e56644fd77269a371225598a818748867ff6
jayapreethi <jayapreethi.sridhar@example.com>
2025-01-30 08:12:20 +0530

    Day-33

---
+++ Day-32/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String[] str = { "Preethi", "Shalini", "Venkat", "Jaya" };
+
+            int LengthofArray = str.Length - 1;
+
+            for (int i = 0; i < LengthofArray; i++)
+            {
+                String temp = str[i];
+                str[i] = str[LengthofArray];
+                str[LengthofArray] = temp;
+                LengthofArray--;
+
+            }
+            foreach (string array in str)
+            {
+                Console.WriteLine(array);
+            }
+        }
+    }
+}
---
+++ Day-33/RotateArray/RotateArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int i = 0, j = numbers.Length - 1,temp;
+            while (i != j)
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+                i++;
+            }
+            //int lastno = numbers.Length;
+            //int Length = numbers.Length-1;
+            //int next = numbers[i];
+            //for (int i = 0; i < Length; i++)
+            //{
+            //    int temp = next;
+            //    next = next+1;
+            //    Length = temp;
+                
+                
+
+            //}
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
