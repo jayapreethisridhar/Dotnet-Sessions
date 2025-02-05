67157821711c444d5d8802616ffea2c61ce4ee05
jayapreethi <jayapreethi.sridhar@example.com>
2025-02-05 22:57:14 +0530

    Day-38

---
+++ Day-38/Solution1/Solution1/SerializationApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using CustomerCommonLib;
+using CustomerCommonLib.Model;
+using System.Text.Json;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SerializeToJson();
+        }
+        private static void SerializeToJson()
+        {
+            var c1 = new Customer { Id = 1, Name = "Preethi",
+            Addresses = new List<Address> {
+            new Address {City="Chennai",Country="India" },
+            new Address {City="Delhi",Country="India"}
+            }
+            };
+            var jsonString = JsonSerializer.Serialize(c1);
+            Console.WriteLine(jsonString);
+            var sw = new StreamWriter("C:\\Jayapreethi-html-app\\Dotnet-Sessions\\cust1.json");
+            sw.WriteLine(jsonString);
+            sw.Close();
+            Console.WriteLine("Written to file");
+        }
+    }
+}
