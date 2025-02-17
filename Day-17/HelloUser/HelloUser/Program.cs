788d95432f765c0b1b9412005c113b938482ed97
jayapreethi <jayapreethi.sridhar@example.com>
2025-02-17 10:59:15 +0530

    Commit

---
+++ Day-17/HelloUser/HelloUser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class HelloUser
+{
+    static void Main()
+    {
+        Console.Write("Enter the number how many times you want to print");
+        var Number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < Number; i++)
+        {
+            Console.WriteLine("Hello RRd user welocome to RRD");
+        }
+    }
+}
+
+
