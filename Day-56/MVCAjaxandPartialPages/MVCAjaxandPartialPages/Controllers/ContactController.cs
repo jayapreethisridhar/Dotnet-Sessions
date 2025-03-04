5f45d9dd07383c91a24d2184dbfb97466437550d
jayapreethisridhar <jayapreethi.sridhar@rrd.com>
2025-03-04 09:42:15 +0530

    updated

---
+++ Day-56/MVCAjaxandPartialPages/MVCAjaxandPartialPages/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MasterApplication.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetTimeStamp()
+        {
+
+            return View("_TimeStamp", new TimeStampDto { Title = "Contacts us Timestampe" });
+
+        }
+    }
+}
---
+++ Day-56/MVCAjaxandPartialPages/MVCAjaxandPartialPages/DTOs/TimeStampDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCAjaxandPartialPages.DTOs
+{
+    public class TimeStampDto
+    {
+        public string Title { get; set; } = "Default";
+        public string Format { get; set; } = "hh:mm:ss";
+    }
+}
